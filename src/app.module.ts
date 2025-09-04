c35abc974b61d08aa23e41ec41f612d67489ba73
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-09-05 00:12:26 +0300

    feat: prepare project for stat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizService } from './quiz/quiz.service';
+import { QuizModule } from './quiz/quiz.module';
 
 @Module({
-  imports: [],
+  imports: [QuizModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, QuizService],
 })
 export class AppModule {}
